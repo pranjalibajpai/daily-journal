812887a75b9e8840b441a2d01ea815ab251fe079
Pranjali Bajpai <2018eeb1243@iitrpr.ac.in>
2020-10-11 01:16:37 +0530

    Added basic functionality

--- server.js
+++ server.js
@@ -1,30 +1,56 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const app = express();
 
 //port setup
 const port = 3000 || process.env.PORT;
 
 //content
-const home_content = "Welcome! to Daily-Journal. Get Started By Creating a new post.";
+const home_content =
+  "Welcome to Daily-Journal. Get Started By Creating a New Post!";
+const about_content =
+  "Hi! I am Pranjali. I am a third year undergrad at IIT Ropar.";
+const contact_content =
+  "Follow me on Linkedin - pranjali-bajpai Github - pranjalibajpai";
+let posts = [];
 
 //middleware bodyparser
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 //ejs template
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-//creates a route to home page & renders home.ejs
-app.get('/', function(request, response){
-    //now we need to add content to home : Pass a JS object
-    response.render("home", {startcontent: home_content});
-    //response.render("home");
-    //response.render(__dirname + '/views/home.ejs');
+//GET requests
+//creates a route to home page & renders home.ejs and pass a JS object to add content
+app.get("/", function (request, response) {
+  response.render("home", { startContent: home_content, posts: posts });
+});
+
+app.get("/about", function (request, response) {
+  response.render("about", { aboutContent: about_content });
+});
+
+app.get("/contact", function (request, response) {
+  response.render("contact", { contactContent: contact_content });
+});
+
+app.get("/compose", function (request, response) {
+  response.render("compose");
+});
+
+//POST requests
+app.post("/compose", function (request, response) {
+  const post = {
+    title: request.body.postTitle,
+    content: request.body.postContent,
+  };
+  posts.push(post);
+  response.redirect("/");
 });
 
 //port connection
-app.listen(port, function(){
-    console.log("Listening to port");
+app.listen(port, function () {
+  console.log("Listening to port");
 });
